d5d76edc70c244a18eb08f33391ef17473a71014
Giulliana  Yamaguchi <tavaresgiulliana@gmail.com>
2022-12-06 13:50:05 -0300

    arquivos do vosk foram adicionados

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Otto
  Assitente virtual em Python 
+
+
+Técnologias: Reconhecimento de voz: (Google para reconhecimento online, Vosk para reconhecimento offline) 
+Síntese de voz
+Algum de tipo IA
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # Our main file.
 
+'''
+                Reconhecimento pelo Google 
+
 import speech_recognition as sr
 
 # Criar um recochecedor
@@ -11,4 +14,28 @@ with sr.Microphone() as source:
         audio = r.listen(source) # Define microfone como fonte de audio
 
         print(r.recognize_google(audio, language='pt'))
-        
+    
+'''
+from vosk import Model, KaldiRecognizer
+import os
+import pyaudio
+
+
+model = Model('model')
+rec = KaldiRecognizer(model, 16000)
+
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
+stream.start_stream()
+
+# Loop do reconhecimento de fala
+while True:
+    data = stream.read(4000)
+    if len(data) == 0:
+        break
+    if rec.AcceptWaveform(data):
+        result = rec.Result()
+        print(rec.Result())
+    else:
+        print(rec.PartialResult())
+print(rec.FinalResult())
